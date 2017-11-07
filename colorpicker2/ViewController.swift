d1af39176f4c6c8224d5ea5894dea95272863ce1
pascaldamiani <pascal.damiani@havasit.com>
2017-11-07 08:14:34 +0100

    Initial Commit

---
+++ colorpicker2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  colorpicker2
+//
+//  Created by devIT on 07/11/2017.
+//  Copyright © 2017 Havasi IT dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
